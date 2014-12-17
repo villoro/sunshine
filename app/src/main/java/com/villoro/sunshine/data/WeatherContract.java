e36fd85d6ee2033a470f7bfa793bbc14a7a126ef
Arnau Villoro <villoro7@gmail.com>
2014-12-17 13:05:44 +0100

    Importats documents lesson 4a

--- app/src/main/java/com/villoro/sunshine/data/WeatherContract.java
+++ app/src/main/java/com/villoro/sunshine/data/WeatherContract.java
@@ -1,12 +1,130 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.villoro.sunshine.data;
 
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
- * Created by Arnau on 25/07/2014.
+ * Defines table and column names for the weather database.
  */
 public class WeatherContract {
 
+    // The "Content authority" is a name for the entire content provider, similar to the
+    // relationship between a domain name and its website.  A convenient string to use for the
+    // content authority is the package name for the app, which is guaranteed to be unique on the
+    // device.
+    /* TODO Uncomment for
+    4b - Adding ContentProvider to our Contract
+    https://www.udacity.com/course/viewer#!/c-ud853/l-1576308909/m-1637521471
+    public static final String CONTENT_AUTHORITY = "com.example.android.sunshine.app";
+
+    // Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+    // the content provider.
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    // Possible paths (appended to base content URI for possible URI's)
+    // For instance, content://com.example.android.sunshine.app/weather/ is a valid path for
+    // looking at weather data. content://com.example.android.sunshine.app/givemeroot/ will fail,
+    // as the ContentProvider hasn't been given any information on what to do with "givemeroot".
+    // At least, let's hope not.  Don't be that dev, reader.  Don't be that dev.
+
+    public static final String PATH_WEATHER = "weather";
+    public static final String PATH_LOCATION = "location";
+    */
+
+    /* TODO Uncomment for
+    4b - Finishing the FetchWeatherTask
+    https://www.udacity.com/course/viewer#!/c-ud853/l-1576308909/m-1675098569
+    // Format used for storing dates in the database.  ALso used for converting those strings
+    // back into date objects for comparison/processing.
+    
+    public static final String DATE_FORMAT = "yyyyMMdd";
+    */
+
+    /**
+     * Converts Date class to a string representation, used for easy comparison and database lookup.
+     * @param date The input date
+     * @return a DB-friendly representation of the date, using the format defined in DATE_FORMAT.
+     */
+    /* TODO Uncomment for
+    4b - Finishing the FetchWeatherTask
+    https://www.udacity.com/course/viewer#!/c-ud853/l-1576308909/m-1675098569
+    public static String getDbDateString(Date date){
+        // Because the API returns a unix timestamp (measured in seconds),
+        // it must be converted to milliseconds in order to be converted to valid date.
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
+        return sdf.format(date);
+    }
+    */
+
+    /**
+     * Converts a dateText to a long Unix time representation
+     * @param dateText the input date string
+     * @return the Date object
+     */
+    /* TODO Uncomment for
+    4b - Finishing the FetchWeatherTask
+    https://www.udacity.com/course/viewer#!/c-ud853/l-1576308909/m-1675098569
+    public static Date getDateFromDb(String dateText) {
+        SimpleDateFormat dbDateFormat = new SimpleDateFormat(DATE_FORMAT);
+        try {
+            return dbDateFormat.parse(dateText);
+        } catch ( ParseException e ) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    */
+
+    public static final class LocationEntry implements BaseColumns {
+        /**
+         * TODO YOUR CODE BELOW HERE FOR QUIZ
+         * QUIZ - 4a - Columns
+         * https://www.udacity.com/course/viewer#!/c-ud853/l-1639338560/e-1633698595/m-1633698597
+         **/
+
+        /* TODO Uncomment for
+        4b - Adding ContentProvider to our Contract
+        https://www.udacity.com/course/viewer#!/c-ud853/l-1576308909/m-1637521471
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_LOCATION).build();
+
+        public static final String CONTENT_TYPE =
+                "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
+        public static final String CONTENT_ITEM_TYPE =
+                "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
+        */
+
+        /**
+         * TODO YOUR CODE BELOW HERE FOR QUIZ
+         * QUIZ - 4b - Adding LocationEntry with ID UriBuilder
+         * https://www.udacity.com/course/viewer#!/c-ud853/l-1576308909/e-1604969848/m-1604969849
+         **/
+
+    }
+
+/*
+/* Inner class that defines the table contents of the weather table */
     public static final class WeatherEntry implements BaseColumns {
 
         public static final String TABLE_NAME = "weather";
@@ -37,19 +155,46 @@ public class WeatherContract {
 
         // Degrees are meteorological degrees (e.g, 0 is north, 180 is south).  Stored as floats.
         public static final String COLUMN_DEGREES = "degrees";
+
+        /* TODO Uncomment for
+        4b - Adding ContentProvider to our Contract
+        https://www.udacity.com/course/viewer#!/c-ud853/l-1576308909/m-1637521471
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_WEATHER).build();
+
+        public static final String CONTENT_TYPE =
+                "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
+        public static final String CONTENT_ITEM_TYPE =
+                "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
+
+        public static Uri buildWeatherUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        public static Uri buildWeatherLocation(String locationSetting) {
+            return CONTENT_URI.buildUpon().appendPath(locationSetting).build();
+        }
+
+        public static Uri buildWeatherLocationWithStartDate(
+                String locationSetting, String startDate) {
+            return CONTENT_URI.buildUpon().appendPath(locationSetting)
+                    .appendQueryParameter(COLUMN_DATETEXT, startDate).build();
+        }
+
+        public static Uri buildWeatherLocationWithDate(String locationSetting, String date) {
+            return CONTENT_URI.buildUpon().appendPath(locationSetting).appendPath(date).build();
+        }
+
+        public static String getLocationSettingFromUri(Uri uri) {
+            return uri.getPathSegments().get(1);
+        }
+
+        public static String getDateFromUri(Uri uri) {
+            return uri.getPathSegments().get(2);
+        }
+
+        public static String getStartDateFromUri(Uri uri) {
+            return uri.getQueryParameter(COLUMN_DATETEXT);
+        }*/
     }
-
-    public static final class LocationEntry implements BaseColumns {
-
-        public static final String TABLE_NAME = "location";
-
-
-        public static final String COLUMN_LOCATION_SETTING = "location_setting";
-        public static final String COLUMN_CITY_NAME = "city_name";
-        public static final String COLUMN_COORD_LAT = "coord_lat";
-        public static final String COLUMN_COORD_LONG = "coord_long";
-
-    }
-
-
 }
--- app/src/main/java/com/villoro/sunshine/data/WeatherDbHelper.java
+++ app/src/main/java/com/villoro/sunshine/data/WeatherDbHelper.java
@@ -1,27 +1,53 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.villoro.sunshine.data;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
-import com.villoro.sunshine.data.WeatherContract.WeatherEntry;
 import com.villoro.sunshine.data.WeatherContract.LocationEntry;
+import com.villoro.sunshine.data.WeatherContract.WeatherEntry;
 
 /**
- * Created by Arnau on 25/07/2014.
+ * Manages a local database for weather data.
  */
 public class WeatherDbHelper extends SQLiteOpenHelper {
 
+    // If you change the database schema, you must increment the database version.
     private static final int DATABASE_VERSION = 1;
+
     public static final String DATABASE_NAME = "weather.db";
 
-    public WeatherDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+    public WeatherDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        /**
+         * TODO YOUR CODE BELOW HERE FOR QUIZ
+         * QUIZ - 4a - LocationEntry
+         * https://www.udacity.com/course/viewer#!/c-ud853/l-1639338560/e-1633698599/m-1633698600
+         **/
 
+        final String SQL_CREATE_LOCATION_TABLE = "";
+
+        /* TODO Uncomment for
+         4a - Create a Database for SQLiteOpenHelper
+         https://www.udacity.com/course/viewer#!/c-ud853/l-1639338560/m-1633698598
         final String SQL_CREATE_WEATHER_TABLE = "CREATE TABLE " + WeatherEntry.TABLE_NAME + " (" +
                 // Why AutoIncrement here, and not above?
                 // Unique keys will be auto-generated in either case.  But for weather
@@ -53,29 +79,25 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
                 " UNIQUE (" + WeatherEntry.COLUMN_DATETEXT + ", " +
                 WeatherEntry.COLUMN_LOC_KEY + ") ON CONFLICT REPLACE);";
 
-
-        final String SQL_CREATE_LOCATION_TABLE = "CREATE TABLE " + LocationEntry.TABLE_NAME + " (" +
-
-                LocationEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-
-                LocationEntry.COLUMN_LOCATION_SETTING + " TEXT UNIQUE NOT NULL, " +
-                LocationEntry.COLUMN_CITY_NAME + " TEXT NOT NULL, " +
-                LocationEntry.COLUMN_COORD_LAT + " REAL NOT NULL, " +
-                LocationEntry.COLUMN_COORD_LONG + " REAL NOT NULL, " +
-                "UNIQUE (" + LocationEntry.COLUMN_LOCATION_SETTING + ") ON CONFLICT IGNORE" + ");";
-
-
-        db.execSQL(SQL_CREATE_LOCATION_TABLE);
-        db.execSQL(SQL_CREATE_WEATHER_TABLE);
-
+        sqLiteDatabase.execSQL(SQL_CREATE_LOCATION_TABLE);
+        sqLiteDatabase.execSQL(SQL_CREATE_WEATHER_TABLE);
+        */
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
-        db.execSQL("DROP TABLE IF EXISTS " + LocationEntry.TABLE_NAME);
-        db.execSQL("DROP TABLE IF EXISTS " + WeatherEntry.TABLE_NAME);
-
-        onCreate(db);
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        /* TODO Uncomment for
+        4a - SQLiteOpenHelper onUpgrade() method
+        https://www.udacity.com/course/viewer#!/c-ud853/l-1639338560/m-1633698602
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        // Note that this only fires if you change the version number for your database.
+        // It does NOT depend on the version number for your application.
+        // If you want to update the schema without wiping data, commenting out the next 2 lines
+        // should be your top priority before modifying this method.
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + LocationEntry.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + WeatherEntry.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+        */
     }
 }
